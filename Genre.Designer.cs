c98495f88b372bcf6cf72477804752c6d20dbe68
vgtom <tom.vieillard.grelet@gmail.com>
2023-01-27 23:04:55 +0100

    Formulaire Genre : Remplacement du MessageBox informatif pour l'ajout, la modification et la suppression d'un genre par un label afin d'éviter les cliques redondant. + modification de l'interface du formulaire Genre

--- Genre.Designer.cs
+++ Genre.Designer.cs
@@ -34,6 +34,9 @@
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grdGenre)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,12 +46,13 @@
             this.grdGenre.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.grdGenre.Location = new System.Drawing.Point(411, 12);
+            this.grdGenre.Location = new System.Drawing.Point(12, 12);
             this.grdGenre.Name = "grdGenre";
             this.grdGenre.RowHeadersWidth = 51;
             this.grdGenre.RowTemplate.Height = 24;
             this.grdGenre.Size = new System.Drawing.Size(456, 616);
             this.grdGenre.TabIndex = 0;
+            this.grdGenre.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdGenre_CellContentClick);
             // 
             // Column1
             // 
@@ -68,9 +72,9 @@
             // 
             this.btnEdit.BackColor = System.Drawing.Color.Yellow;
             this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEdit.Location = new System.Drawing.Point(74, 246);
+            this.btnEdit.Location = new System.Drawing.Point(551, 430);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(249, 156);
+            this.btnEdit.Size = new System.Drawing.Size(249, 61);
             this.btnEdit.TabIndex = 1;
             this.btnEdit.Text = "Modifier";
             this.btnEdit.UseVisualStyleBackColor = false;
@@ -80,9 +84,9 @@
             // 
             this.btnDelete.BackColor = System.Drawing.Color.Red;
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Location = new System.Drawing.Point(74, 424);
+            this.btnDelete.Location = new System.Drawing.Point(551, 521);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(249, 156);
+            this.btnDelete.Size = new System.Drawing.Size(249, 61);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Supprimer";
             this.btnDelete.UseVisualStyleBackColor = false;
@@ -92,20 +96,48 @@
             // 
             this.btnAdd.BackColor = System.Drawing.Color.YellowGreen;
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(74, 65);
+            this.btnAdd.Location = new System.Drawing.Point(551, 336);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(249, 156);
+            this.btnAdd.Size = new System.Drawing.Size(249, 62);
             this.btnAdd.TabIndex = 5;
             this.btnAdd.Text = "Ajouter";
             this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(551, 186);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(248, 22);
+            this.txtGenre.TabIndex = 6;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(559, 221);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(44, 16);
+            this.lblMsg.TabIndex = 7;
+            this.lblMsg.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(548, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Libellé de genre";
+            // 
             // Genre
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackColor = System.Drawing.Color.CadetBlue;
             this.ClientSize = new System.Drawing.Size(879, 640);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.txtGenre);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
@@ -115,6 +147,7 @@
             this.Load += new System.EventHandler(this.Genre_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grdGenre)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -126,5 +159,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Genre.cs
+++ Genre.cs
@@ -15,15 +15,20 @@ namespace AP_CINE_APPLI
     public partial class Genre : Form
     {
         string pwdDb = "root";
+        
+
         public Genre()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
+            lblMsg.Text = "";
         }
 
         private void Genre_Load(object sender, EventArgs e)
         {
+            lblMsg.BackColor = Color.White;
+
             grdGenre.AllowUserToAddRows = false;
             grdGenre.ReadOnly = true;
             grdGenre.RowHeadersVisible = false;
@@ -58,55 +63,115 @@ namespace AP_CINE_APPLI
             cnn.Close();
 
             grdGenre.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
-
             grdGenre.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-
         }
+
+        private bool checkExistGenre(string libgenre)
+        {
+            lblMsg.Text = "";
+            lblMsg.ForeColor = Color.Black;
+            bool existengenre = false;
+            for (int i = 0; i < grdGenre.Rows.Count; i++)
+            {
+                if (grdGenre[1, i].Value.ToString() == libgenre)
+                {
+                    existengenre = true;
+                    lblMsg.Text = "Ce genre existe déjà";
+                    lblMsg.ForeColor = Color.Red;
+                    break;
+                }
+            }
+            return existengenre;
+        }
+
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            string nomGenre = Interaction.InputBox("Saisissez le libellé du nouveau genre :");
-            if (nomGenre!="")
+            if (txtGenre.Text.ToString() != "")
+            { 
+                if (!checkExistGenre(txtGenre.Text.ToString()))
+                {
+                    OdbcConnection cnn = new OdbcConnection();
+                    OdbcCommand cmd = new OdbcCommand();
+
+                    cnn.ConnectionString = "Driver={MySQL ODBC 8.0 ANSI Driver};SERVER=localhost;Database=bdcinevieillard-lepers;uid=root;pwd=" + pwdDb + "";
+                    cnn.Open();
+
+                    cmd.CommandText = "insert into genre values (null, '" + txtGenre.Text.ToString() + "')";
+                    cmd.Connection = cnn;
+                    cmd.ExecuteReader();
+
+                    cnn.Close();
+
+                    lblMsg.Text = "Le genre \"" + txtGenre.Text.ToString() + "\" a été ajouté";
+                    lblMsg.ForeColor = Color.Blue;
+
+                    Genre_Load(sender, e);
+                }
+            }
+            else
             {
-                OdbcConnection cnn = new OdbcConnection();
-                OdbcCommand cmd = new OdbcCommand();
-
-                cnn.ConnectionString = "Driver={MySQL ODBC 8.0 ANSI Driver};SERVER=localhost;Database=bdcinevieillard-lepers;uid=root;pwd=" + pwdDb + "";
-                cnn.Open();
-
-                cmd.CommandText = "insert into genre values (null, '" + nomGenre + "')";
-                cmd.Connection = cnn;
-                cmd.ExecuteReader();
-
-                cnn.Close();
-                MessageBox.Show("Le genre \"" + nomGenre + "\" a été ajouté");
-                Genre_Load(sender, e);
+                lblMsg.Text = "Libellé de genre invalide";
+                lblMsg.ForeColor = Color.Red;
             }
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            if (grdGenre.RowCount > 0 && MessageBox.Show("Êtes-vous sûr de vouloir modifier le libellé du genre suivant :\n" + grdGenre[1, grdGenre.CurrentRow.Index].Value, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            // ANCIENNE MÉTHODE
+            //if (grdGenre.RowCount > 0 && MessageBox.Show("Êtes-vous sûr de vouloir modifier le libellé du genre suivant :\n" + grdGenre[1, grdGenre.CurrentRow.Index].Value, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            //{
+            //    string newNameGenre = Interaction.InputBox("Saisissez le nouveau libellé de " + grdGenre[1, grdGenre.CurrentRow.Index].Value);
+
+            //    if (newNameGenre!="")
+            //    {
+            //        MessageBox.Show("Le libellé du genre \"" + grdGenre[1, grdGenre.CurrentRow.Index].Value + "\" a été modifié en " + newNameGenre);
+
+            //        OdbcConnection cnn = new OdbcConnection();
+
+            //        cnn.ConnectionString = "Driver={MySQL ODBC 8.0 ANSI Driver};SERVER=localhost;Database=bdcinevieillard-lepers;uid=root;pwd=" + pwdDb + "";
+            //        cnn.Open();
+
+            //        OdbcCommand cmdfilm = new OdbcCommand(); OdbcDataReader drrfilm;
+            //        cmdfilm.CommandText = "update genre set libgenre = '" + newNameGenre + "' where nogenre =" + grdGenre[0, grdGenre.CurrentRow.Index].Value + "";
+            //        cmdfilm.Connection = cnn;
+            //        drrfilm = cmdfilm.ExecuteReader();
+
+            //        drrfilm.Close();
+
+            //        Genre_Load(sender, e);
+            //    }
+            //}
+
+            if (grdGenre.RowCount >= 0)
             {
-                string newNameGenre = Interaction.InputBox("Saisissez le nouveau libellé de " + grdGenre[1, grdGenre.CurrentRow.Index].Value);
-                
-                if (newNameGenre!="")
+
+                if (txtGenre.Text.ToString() != "")
                 {
-                    MessageBox.Show("Le libellé du genre \"" + grdGenre[1, grdGenre.CurrentRow.Index].Value + "\" a été modifié en " + newNameGenre);
+                    if (!checkExistGenre(txtGenre.Text.ToString()))
+                    {
+                        OdbcConnection cnn = new OdbcConnection();
 
-                    OdbcConnection cnn = new OdbcConnection();
+                        cnn.ConnectionString = "Driver={MySQL ODBC 8.0 ANSI Driver};SERVER=localhost;Database=bdcinevieillard-lepers;uid=root;pwd=" + pwdDb + "";
+                        cnn.Open();
 
-                    cnn.ConnectionString = "Driver={MySQL ODBC 8.0 ANSI Driver};SERVER=localhost;Database=bdcinevieillard-lepers;uid=root;pwd=" + pwdDb + "";
-                    cnn.Open();
+                        OdbcCommand cmdfilm = new OdbcCommand(); OdbcDataReader drrfilm;
+                        cmdfilm.CommandText = "update genre set libgenre = '" + txtGenre.Text.ToString() + "' where nogenre =" + grdGenre[0, grdGenre.CurrentRow.Index].Value + "";
+                        cmdfilm.Connection = cnn;
+                        drrfilm = cmdfilm.ExecuteReader();
 
-                    OdbcCommand cmdfilm = new OdbcCommand(); OdbcDataReader drrfilm;
-                    cmdfilm.CommandText = "update genre set libgenre = '" + newNameGenre + "' where nogenre =" + grdGenre[0, grdGenre.CurrentRow.Index].Value + "";
-                    cmdfilm.Connection = cnn;
-                    drrfilm = cmdfilm.ExecuteReader();
+                        drrfilm.Close();
 
-                    drrfilm.Close();
+                        lblMsg.Text = "Le genre \"" + grdGenre[1, grdGenre.CurrentRow.Index].Value + "\" a été modifié en \"" + txtGenre.Text.ToString() + "\"";
+                        lblMsg.ForeColor = Color.Blue;
 
-                    Genre_Load(sender, e);
+                        Genre_Load(sender, e);
+                    }
+                }
+                else
+                {
+                    lblMsg.Text = "Libellé de genre invalide";
+                    lblMsg.ForeColor = Color.Red;
                 }
             }
         }
@@ -135,10 +200,16 @@ namespace AP_CINE_APPLI
                 drrfilm = cmdfilm.ExecuteReader();
 
                 drrfilm.Close();
-                MessageBox.Show("Le genre \"" + grdGenre[1, grdGenre.CurrentRow.Index].Value + "\" a été supprimé");
+                lblMsg.Text = "Le genre \"" + grdGenre[1, grdGenre.CurrentRow.Index].Value + "\" a été supprimé";
+                lblMsg.ForeColor = Color.Red;
 
                 Genre_Load(sender, e);
             }
+        }
+
+        private void grdGenre_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtGenre.Text = grdGenre[1, grdGenre.CurrentCell.RowIndex].Value.ToString();
         }
     }
 }
